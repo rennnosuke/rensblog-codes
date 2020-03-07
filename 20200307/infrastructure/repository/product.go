ea87452cac748a352da6bae7e18ace0cef2dfaae
RenKanai <turutekab@gmail.com>
2020-03-07 15:51:37 +0900

    modify 20200307: timeout

--- 20200307/infrastructure/repository/product.go
+++ 20200307/infrastructure/repository/product.go
@@ -2,12 +2,31 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"github.com/rennnosuke/rens-blog-codes/20200307/domain/entity"
+	"time"
 )
 
 type ProductRepositoryImpl struct{}
 
 func (r *ProductRepositoryImpl) GetProducts(ctx context.Context) ([]entity.Product, error) {
+
+	c := make(chan []entity.Product, 1)
+	childCtx, _ := context.WithTimeout(ctx, time.Second*5)
+
+	go func(ctx context.Context) { c <- r.FindProducts(ctx) }(childCtx)
+
+	select {
+	case <-childCtx.Done():
+		<-c
+		return nil, errors.New("query canceled")
+	case prods := <-c:
+		return prods, nil
+	}
+
+}
+
+func (r *ProductRepositoryImpl) FindProducts(ctx context.Context) []entity.Product {
 	return []entity.Product{
 		{
 			Name:  "Chocolate",
@@ -21,5 +40,5 @@ func (r *ProductRepositoryImpl) GetProducts(ctx context.Context) ([]entity.Produ
 			Name:  "Candy",
 			Price: 100,
 		},
-	}, nil
+	}
 }
